53fd69d50a802beb7406407919ef436ceb9716ea
Darina <darina460@gmail.com>
2022-03-19 19:08:50 +0200

    first comit

---
+++ script.js
@@ -0,0 +1,21 @@
+// function onButtonClick(){
+//     const elem = document.getElementById("input");
+    
+//     addMessage(elem.value);
+// }
+// function onButtonClick2(){
+//     const elem = document.getElementById("input");
+//     removeMessage(elem);
+// }
+// function addMessage(message){
+//     const list = document.getElementById("messages");
+//     const newElem = document.createElement("li");
+//     newElem.textContent = message;
+//     list.appendChild(newElem);   
+// }
+// function removeMessage(message){
+//     const list = document.getElementById("messages");
+//     const newElem = document.getElementById("li");
+//     newElem.textContent = message;
+//     newElem.removeChild(newElem);
+// }
